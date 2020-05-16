2154b798b5f47dbe90c4271b9db0f42401b74e7a
no-brand <do.dream.david@gmail.com>
2020-05-16 21:09:25 +0900

    feat(tf): init aws provider

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_shared_credentials_file
+  profile                 = var.aws_profile
+}
+
+data "aws_caller_identity" "current" {}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+}
+
+variable "aws_shared_credentials_file" {
+  description = "aws credentials path"
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "aws profile"
+  type        = string
+}
